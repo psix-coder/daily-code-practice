3bda8496730ab6cbd36d9b05c32be8b117d81790
Abdulloh Ahmadjonov <siddiquzb@gmail.com>
2025-10-05 23:05:27 +0500

    Day 2: Add Reverse String problem solution

---
+++ strings/reserve_string.py
@@ -0,0 +1,33 @@
+# def reverse_string(s):
+#     """
+#     Listni teskari qiladi (in-place)
+#     """
+#     pass
+
+
+# # Test
+# if __name__ == "__main__":
+#     # Test 1
+#     s1 = ["h","e","l","l","o"]
+#     reverse_string(s1)
+#     print(s1)  # ["o","l","l","e","h"] bo'lishi kerak
+    
+
+
+
+
+
+# s1
+# def reserve_string(s):
+#     left = 0
+#     right = len(s) - 1
+
+#     while left < right:
+#         s[left], s[right] = s[right], s[left]
+#         left += 1
+#         right -= 1
+
+# s1 = ["h","e","l","l","o"]
+# reserve_string(s1)
+# print(s1)
+
